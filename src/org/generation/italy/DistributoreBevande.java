30d2b6945bc09bc192fecf39d180de76031fc7fd
SaraSgarlata <sara-04@live.it>
2023-11-25 14:53:57 +0100

    terminato ma da modificare

--- src/org/generation/italy/DistributoreBevande.java
+++ src/org/generation/italy/DistributoreBevande.java
@@ -5,35 +5,55 @@ import java.util.Scanner;
 
 public class DistributoreBevande {
 	Scanner sc = new Scanner (System.in);
-	private String bevanda;
-	private Float credito;
 	
+	 float credito =0;
+	 	
 	
+		
+	HashMap <String, Float> bevanda = new HashMap<>() {{
+	
+	put("Fanta:\t", 1.50f);
+	put("Coca:\t", 1.50f);
+	put("Coca Zero:", 2.00f);
+	put("Sprite:\t", 1.50f);
+	put("Ace:\t", 2.50f);
+
+	}};
+
+			
 	public void elencoBevande () {
 		
-	HashMap <String, Float> bevanda = new HashMap<>();
+		for (String n:bevanda.keySet())	
+			System.out.println(n + " " + bevanda.get(n) + "€");
+			}
 	
-	bevanda.put("Fanta:\t", 1.50f);
-	bevanda.put("Coca:\t", 1.50f);
-	bevanda.put("Coca Zero:", 2.00f);
-	bevanda.put("Sprite:\t", 1.50f);
-	bevanda.put("Ace:\t", 2.50f);
-	
-	for (String n:bevanda.keySet())	
-	System.out.println(n + " " + bevanda.get(n) + "€");
-	}
-				
 	public void inserisciMoneta (Float moneta) {
 	
 		if (moneta==0.50f)
-			credito+=0.50f;
+			credito+=0.50;
 		else if (moneta==1.00f)
-			credito+=1.00f;
+			credito+=1.00;
 		else if (moneta==2.00f)
-			credito+=2.00f;					
+			credito+=2.00;					
 	}
 	
-	public void erogaBevanda () {
+	public void visualizzaCredito () {
+		System.out.println("Il credito inserito è :"+ credito + "€\n");
+		
+	}
+	public void erogaBevanda (String bevandaScelta1) {
+		if(bevanda.containsKey(bevandaScelta1))
+			
+		if (credito>=bevanda.get(bevandaScelta1)){
+				
+				credito-=bevanda.get(bevandaScelta1);
+		
+		System.out.println("la tua bevanda è stata erogata");
+		
+			}
+			
+			else System.out.println("credito non sufficiente");
+			else System.out.println("prodotto non disponibile");
 		
 	}
 }
--- src/org/generation/italy/Main.java
+++ src/org/generation/italy/Main.java
@@ -8,17 +8,42 @@ public class Main {
 		// TODO Auto-generated method stub
 
 		Scanner sc = new Scanner (System.in);
-		Float moneta;
+		float moneta;
+		String risposta, bevandaScelta;
 		
 		DistributoreBevande distributore = new DistributoreBevande();
 				
 		System.out.println("Quale bevanda desideri? ecco le disponibilità \n");
-		distributore.elencoBevande();
+		distributore.elencoBevande();//nome classe + nome metodo che stampa la lista e la scorre con for each
 		
+		do {
+			
 		System.out.println("\nPuoi inserire monete da: 0.50/1.00/2.00 €");
 		moneta=Float.parseFloat(sc.nextLine());
-		distributore.inserisciMoneta(moneta);
+		distributore.inserisciMoneta(moneta);//nome classe + nome metodo che calcola i soldi inseriti
 		
+		distributore.visualizzaCredito();//nome classe e metodo che visualizza il credito inserito
+		
+		System.out.println("Vuoi inserire un'altra moneta?");
+		risposta=sc.nextLine();
+		}
+		while(risposta.equals("si"));
+		
+		do {
+		System.out.println("Seleziona la bevanda desiderata: ");
+		bevandaScelta=sc.nextLine().toLowerCase();
+		
+		System.out.println("Il tuo credito è: ");
+		distributore.visualizzaCredito();//mi dice il credito iniziale, da cambiare errore
+		
+		System.out.println("Vuoi prendere un'altra bevanda?");
+		risposta=sc.nextLine();
+		}
+		while(risposta.equals("si"));
+		
+		System.out.println("Il tuo resto è:");
+		distributore.visualizzaCredito();//errore da cambiare
+		System.out.println("Grazie e arrivederci");
 		
 		
 	}
