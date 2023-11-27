93bfa130ecb19fc24fae27d54ed9d5691276799c
Andrea Cuccuru <cuccuruandrea16@gmail.com>
2023-11-27 11:40:07 +0100

    aggiunto costruttore

--- src/org/generation/italy/DistributoreBevande.java
+++ src/org/generation/italy/DistributoreBevande.java
@@ -4,36 +4,50 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class DistributoreBevande {
-	Scanner sc = new Scanner (System.in);
-	private String bevanda;
-	private Float credito;
 	
+
+	private float credito;
+	private HashMap<String, Float> bevande = new HashMap<>();	
 	
-	public void elencoBevande () {
-		
-	HashMap <String, Float> bevanda = new HashMap<>();
-	
-	bevanda.put("Fanta:\t", 1.50f);
-	bevanda.put("Coca:\t", 1.50f);
-	bevanda.put("Coca Zero:", 2.00f);
-	bevanda.put("Sprite:\t", 1.50f);
-	bevanda.put("Ace:\t", 2.50f);
-	
-	for (String n:bevanda.keySet())	
-	System.out.println(n + " " + bevanda.get(n) + "€");
+
+	public DistributoreBevande() { 
+
+		bevande.put("Fanta", 1.20f);
+		bevande.put("Coca", 1.50f);
+		bevande.put("Coca Zero", 2.00f);
+		bevande.put("Sprite", 1.50f);
+		bevande.put("Ace", 2.50f);
+		credito=0;
 	}
-				
-	public void inserisciMoneta (Float moneta) {
-	
-		if (moneta==0.50f)
-			credito+=0.50f;
-		else if (moneta==1.00f)
-			credito+=1.00f;
-		else if (moneta==2.00f)
-			credito+=2.00f;					
+
+	public void elencoBevande() {
+
+		for (String n : bevande.keySet())
+			System.out.println(n + " " + bevande.get(n) + "€");
 	}
-	
-	public void erogaBevanda () {
+
+	public void inserisciMoneta(Float moneta) {
+
+		if (moneta == 0.20 || moneta == 0.50 || moneta == 1.00 || moneta == 2.00)
+			credito += moneta;
+	}
+
+	public void visualizzaCredito() {
+		System.out.println("credito: " + credito + "€");
+
+	}
+
+	public void erogaBevanda(String nomeBevanda) {
+		if (bevande.containsKey(nomeBevanda)) {
+			if (credito >= bevande.get(nomeBevanda)) {
+				credito -= bevande.get(nomeBevanda);
+				System.out.println("la tua bevanda:");
+				System.out.println("il tuo resto: "+String.format("%.2f€", credito));
+				credito=0;
+			} else
+				System.out.println("credito non sufficente");
+		} else
+			System.out.println("prodotto non disponibile");
 		
 	}
 }
--- src/org/generation/italy/Main.java
+++ src/org/generation/italy/Main.java
@@ -5,22 +5,37 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 		Scanner sc = new Scanner (System.in);
 		Float moneta;
-		
+		String risposta,bevandaScelta;
 		DistributoreBevande distributore = new DistributoreBevande();
-				
+		
+		
 		System.out.println("Quale bevanda desideri? ecco le disponibilità \n");
 		distributore.elencoBevande();
 		
-		System.out.println("\nPuoi inserire monete da: 0.50/1.00/2.00 €");
-		moneta=Float.parseFloat(sc.nextLine());
-		distributore.inserisciMoneta(moneta);
+		do {
+			System.out.println("\nPuoi inserire monete da: 0.50/1.00/2.00 €");
+			moneta=Float.parseFloat(sc.nextLine());
+			distributore.inserisciMoneta(moneta);
+			distributore.visualizzaCredito();
+			System.out.println("vuoi inserire altre monete?");
+			risposta=sc.nextLine();
+		} while(risposta.equals("si"));
+		
+		do {
+			System.out.println("scegli una bevanda");
+			bevandaScelta=sc.nextLine();
+			distributore.erogaBevanda(bevandaScelta);
+			System.out.println("il tuo credito ora: ");
+			distributore.visualizzaCredito();
+			System.out.println("vuoi altro?");
+			risposta=sc.nextLine();
+		} while(risposta.equals("si"));
 		
 		
-		
+		System.out.println("Arrivederci");
 	}
 
 }
